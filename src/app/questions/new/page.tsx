c64f29b64ac5b8b68016cd5fb36df65ee9df193b
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-05-09 16:42:26 +0500

    Handle form submission

--- src/app/questions/new/page.tsx
+++ src/app/questions/new/page.tsx
@@ -1,16 +1,55 @@
 'use client';
 
 import { Button, TextField } from '@radix-ui/themes';
+import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
+import { useRouter } from 'next/navigation';
+import { useMemo } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import SimpleMDE from 'react-simplemde-editor';
 
+interface NewQuestionForm {
+  title: string;
+  description: string;
+}
+
 const NewQuestionPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<NewQuestionForm>();
+
+  const onSubmit = async (data: NewQuestionForm) => {
+    await axios.post('/api/questions', data);
+    router.push('/questions');
+  }
+
+  const autofocusNoSpellcheckerOptions = useMemo(() => {
+    return {
+      autofocus: true,
+      spellChecker: false,
+    };
+  }, []);
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title" />
-      <SimpleMDE placeholder="Description" />;
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register('title')}
+      />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE
+            {...field}
+            options={autofocusNoSpellcheckerOptions}
+          />
+        )}
+      />
       <Button>Submit New Question</Button>
-    </div>
+    </form>
   );
 };
 export default NewQuestionPage;
