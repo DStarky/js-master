e1b10055ac848f3385801a19be85cf299b6d0b04
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-05-09 23:52:54 +0500

    Show the questions

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,12 +1,60 @@
-import { Button } from '@radix-ui/themes';
+import { Button, Table } from '@radix-ui/themes';
 import Link from 'next/link';
 
-const QuestionPage = () => {
+import prisma from '../../../prisma/client';
+
+const QuestionPage = async () => {
+  const questions = await prisma.question.findMany();
+
   return (
     <div>
-      <Link href="/questions/new">
-        <Button>New Question</Button>
-      </Link>
+      <div className="mb-5">
+        <Link href="/questions/new">
+          <Button>New Question</Button>
+        </Link>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Complexity
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {questions.map(question => (
+            <Table.Row key={question.id}>
+              <Table.Cell>
+                {question.title}
+                <div className="mt-2 block md:hidden">{question.status}</div>
+              </Table.Cell>
+              <Table.Cell>
+                {question.description}
+                <div className="mt-2 block md:hidden">
+                  {question.complexity}
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {question.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {question.complexity}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {question.createdAt.toLocaleDateString('ru-RU')}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
