9cd392ff235891b2c80ab7db327b007bd476049e
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-05-10 23:11:07 +0500

    Add the edit button

--- src/app/questions/[id]/page.tsx
+++ src/app/questions/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import Markdown from 'react-markdown';
 
@@ -20,23 +22,36 @@ const QuestionDetailPage = async ({ params }: QuestionDetailPageProps) => {
   if (!question) notFound();
 
   return (
-    <div>
-      <Heading>{question.title}</Heading>
-      <Flex
-        gap="3"
-        my="2"
-      >
-        <QuestionStatusBadge status={question.status} />
-        <QuestionComplexityBadge complexity={question.complexity} />
-        <Text>{question.createdAt.toLocaleDateString('ru-RU')}</Text>
-      </Flex>
-      <Card
-        className="prose"
-        mt="4"
-      >
-        <Markdown>{question.description}</Markdown>
-      </Card>
-    </div>
+    <Grid
+      columns={{ initial: '1', md: '2' }}
+      gap="5"
+    >
+      <Box>
+        <Heading>{question.title}</Heading>
+        <Flex
+          gap="3"
+          my="2"
+        >
+          <QuestionStatusBadge status={question.status} />
+          <QuestionComplexityBadge complexity={question.complexity} />
+          <Text>{question.createdAt.toLocaleDateString('ru-RU')}</Text>
+        </Flex>
+        <Card
+          className="prose"
+          mt="4"
+        >
+          <Markdown>{question.description}</Markdown>
+        </Card>
+      </Box>
+      <Box>
+        <Link href={`/questions/${question.id}/edit`}>
+          <Button>
+            <Pencil2Icon />
+            Edit Question
+          </Button>
+        </Link>
+      </Box>
+    </Grid>
   );
 };
 export default QuestionDetailPage;
