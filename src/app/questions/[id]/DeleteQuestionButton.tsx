6c0617264d65abfbbe4f281a2fd573ed4a2da22f
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-05-11 15:52:57 +0500

    Handle errors

--- src/app/questions/[id]/DeleteQuestionButton.tsx
+++ src/app/questions/[id]/DeleteQuestionButton.tsx
@@ -3,69 +3,92 @@
 import { TrashIcon } from '@radix-ui/react-icons';
 import { AlertDialog, Button } from '@radix-ui/themes';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 import { deleteQuestion } from '@/service/questionService';
 
 const DeleteQuestionButton = ({ questionId }: { questionId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState<boolean>(false);
+
   const deleteHandler = async (questionId: number) => {
     try {
       await deleteQuestion(questionId);
       router.push('/questions');
       router.refresh();
     } catch (error) {
-      console.log(error);
+      setError(true);
     }
   };
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button
-          color="red"
-          className="cursor-pointer"
-        >
-          <TrashIcon /> Delete Question
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content className="AlertDialogContent">
-        <AlertDialog.Title className="AlertDialogTitle">
-          Are you absolutely sure?
-        </AlertDialog.Title>
-        <AlertDialog.Description className="AlertDialogDescription">
-          This action cannot be undone. This will permanently delete question
-          and remove your data from our servers.
-        </AlertDialog.Description>
-        <div
-          style={{
-            display: 'flex',
-            gap: 25,
-            justifyContent: 'flex-end',
-            marginTop: 25,
-          }}
-        >
-          <AlertDialog.Cancel>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button
+            color="red"
+            className="cursor-pointer"
+          >
+            <TrashIcon /> Delete Question
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content className="AlertDialogContent">
+          <AlertDialog.Title className="AlertDialogTitle">
+            Are you absolutely sure?
+          </AlertDialog.Title>
+          <AlertDialog.Description className="AlertDialogDescription">
+            This action cannot be undone. This will permanently delete question
+            and remove your data from our servers.
+          </AlertDialog.Description>
+          <div
+            style={{
+              display: 'flex',
+              gap: 25,
+              justifyContent: 'flex-end',
+              marginTop: 25,
+            }}
+          >
+            <AlertDialog.Cancel>
+              <Button
+                className="cursor-pointer"
+                color="gray"
+                variant="soft"
+              >
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button
+                color="red"
+                className="cursor-pointer"
+                variant="soft"
+                onClick={() => deleteHandler(questionId)}
+              >
+                Yes, delete question
+              </Button>
+            </AlertDialog.Action>
+          </div>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Something went wrong...</AlertDialog.Title>
+          <AlertDialog.Description>
+            This question could not be deleted. Please try again later
+          </AlertDialog.Description>
+          <div className="flex justify-end">
             <Button
-              className="cursor-pointer"
+              className="mt-5 cursor-pointer text-center"
               color="gray"
               variant="soft"
+              onClick={() => setError(false)}
             >
-              Cancel
+              OK
             </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              className="cursor-pointer"
-              variant="soft"
-              onClick={() => deleteHandler(questionId)}
-            >
-              Yes, delete question
-            </Button>
-          </AlertDialog.Action>
-        </div>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+          </div>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 export default DeleteQuestionButton;
