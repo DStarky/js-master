135d8178b9d62bc12d1e560b9a039b597ac8ca00
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-05-11 15:24:27 +0500

    Show a confirmation dialog box

--- src/app/questions/[id]/DeleteQuestionButton.tsx
+++ src/app/questions/[id]/DeleteQuestionButton.tsx
@@ -1,11 +1,56 @@
+'use client';
+
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button } from '@radix-ui/themes';
 
 const DeleteQuestionButton = ({ questionId }: { questionId: number }) => {
   return (
-    <Button color="red">
-      <TrashIcon /> Delete Question
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button
+          color="red"
+          className="cursor-pointer"
+        >
+          <TrashIcon /> Delete Question
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content className="AlertDialogContent">
+        <AlertDialog.Title className="AlertDialogTitle">
+          Are you absolutely sure?
+        </AlertDialog.Title>
+        <AlertDialog.Description className="AlertDialogDescription">
+          This action cannot be undone. This will permanently delete question
+          and remove your data from our servers.
+        </AlertDialog.Description>
+        <div
+          style={{
+            display: 'flex',
+            gap: 25,
+            justifyContent: 'flex-end',
+            marginTop: 25,
+          }}
+        >
+          <AlertDialog.Cancel>
+            <Button
+              className="cursor-pointer"
+              color="gray"
+              variant="soft"
+            >
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button
+              color="red"
+              className="cursor-pointer"
+              variant="soft"
+            >
+              Yes, delete question
+            </Button>
+          </AlertDialog.Action>
+        </div>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 export default DeleteQuestionButton;
