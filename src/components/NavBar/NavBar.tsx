6d78f098a5834e2a2c15dcb2ce73585bfa62d1ef
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-05-11 22:44:16 +0500

    Change the layout of the NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,6 @@
-import { Box } from '@radix-ui/themes';
+'use client';
+
+import { Box, Container } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { AiFillBug } from 'react-icons/ai';
@@ -8,17 +10,25 @@ import NavBarLinks from './NavBarLinks';
 const NavBar = () => {
   const { status, data: session } = useSession();
   return (
-    <nav className="mb-5 flex h-14 items-center space-x-6 border-b px-5">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <NavBarLinks />
-      <Box>
-        {status === 'authenticated' && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-        {status === 'unauthenticated' && <Link href="/api/auth/signin">Log in</Link>}
-      </Box>
+    <nav className="mb-5 border-b px-5">
+      <Container>
+        <div className="flex h-14 items-center justify-between ">
+          <div className="flex items-center space-x-6">
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <NavBarLinks />
+          </div>
+          <Box className="">
+            {status === 'authenticated' && (
+              <Link href="/api/auth/signout">Log out</Link>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href="/api/auth/signin">Log in</Link>
+            )}
+          </Box>
+        </div>
+      </Container>
     </nav>
   );
 };
